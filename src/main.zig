d424753f3791678753e037f7d793f9fc1f0040e9
David Kouřil <david.kouril@gmail.com>
2024-01-22 18:01:43 -0500

    trying to output ppm

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,50 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("hey.", .{});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+    const image_width = 256;
+    const image_height = 256;
+
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("P3\n{} {}\n255\n", .{ image_width, image_height });
 
-    try bw.flush(); // don't forget to flush!
+    for (0..image_height - 1) |_| {
+        for (0..image_width - 1) |_| {
+            // const r = i / (image_width - 1);
+            // const g = j / (image_height - 1);
+            // const b = 0;
+
+            // const outR = r * 255;
+            // const outG = g * 255;
+            // const outB = b * 255;
+            const outR = 255;
+            const outG = 255;
+            const outB = 255;
+
+            try stdout.print("{} {} {}\n", .{ outR, outG, outB });
+        }
+    }
 }
+
+// pub fn main() !void {
+//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+//
+//     // stdout is for the actual output of your application, for example if you
+//     // are implementing gzip, then only the compressed bytes should be sent to
+//     // stdout, not any debugging messages.
+//     const stdout_file = std.io.getStdOut().writer();
+//     var bw = std.io.bufferedWriter(stdout_file);
+//     const stdout = bw.writer();
+//
+//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+//
+//     try bw.flush(); // don't forget to flush!
+// }
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
